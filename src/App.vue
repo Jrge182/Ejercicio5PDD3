f0394e2e2c556ce885a29c4810d6ed3e0b4fe761
Jorge <91501563+Jrge182@users.noreply.github.com>
2023-11-02 23:38:04 -0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Posts</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        <router-link :to="`/posts/${post.id}`">
+          {{ post.title }}
+        </router-link>
+      </li>
+    </ul>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        this.posts = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
+</script>
---
+++ src/Posts.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{ post.title }}</h2>
+    <p>{{ post.body }}</p>
+    <p>{{ post.userId }}</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Posts",
+  props: {
+    post: {
+      type: Object,
+      required: true,
+    },
+  },
+});
+</script>
